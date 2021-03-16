93498ef864c8b43dae426293dc20afe79ccd7efc
lpescador <lucaspescadordecamargo@gmail.com>
2021-03-16 13:39:49 -0300

    Criacao Veiculo

---
+++ Veiculo.java
@@ -0,0 +1,52 @@
+public class Veiculo {
+    
+    private String placa;
+    private String marca;
+    private String modelo;
+    private int ano;
+    private Double valor;
+
+    public Veiculo(String placa) {
+        this.placa = placa;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+}
